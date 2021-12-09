bd33c6c1984c539a19fad8eab95c5916afaafb8b
Claudi0-V <claudiov.dejesus@protonmail.com>
2021-12-09 20:45:03 -0300

    added basic structure

--- app.js
+++ app.js
@@ -5,25 +5,20 @@ const path = require("path");
 const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const mongoDb = "YOUR MONGO URL HERE";
-mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "mongo connection error"));
-
-const User = mongoose.model(
-  "User",
-  new Schema({
-    username: { type: String, required: true },
-    password: { type: String, required: true }
-  })
-);
 
 const app = express();
 app.set("views", __dirname);
 app.set("view engine", "ejs");
+
+const mongoose = require("mongoose");
+const dbURI = process.env.DBURI;
+mongoose
+  .connect(dbURI, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then((result) => {
+    app.listen(3000, () => console.log("app listening on port 3000!"));
+  });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongo connection error"));
 
 app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
@@ -31,5 +26,3 @@ app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => res.render("index"));
-
-app.listen(3000, () => console.log("app listening on port 3000!"));
